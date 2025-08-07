ccb9a2706445cc4b5e6a5a55fee3485ca92d26ed
Rabibola <hs931623@gmail.com>
2025-08-07 05:39:33 -0300

    Declarando as variáveis para as cartas

---
+++ CartasSuperTrunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main() {
+    // Declaração das variáveis para a Carta 1
+    char estado1, codigo1[5], nomeCidade1[50];
+    int populacao1, pontosTuristicos1;
+    float area1, pib1;
+
+    // Declaração das variáveis para a Carta 2
+    char estado2, codigo2[5], nomeCidade2[50];
+    int populacao2, pontosTuristicos2;
+    float area2, pib2;
+
+    return 0;
+}
